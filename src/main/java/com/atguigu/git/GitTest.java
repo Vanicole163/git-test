7d48c03fb8888c48acddf399b117b2c830cb4751
Van <Van@163.com>
2022-11-17 21:27:24 +0800

    for push to github

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello idea");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
+        System.out.println("hello tired");
+
     }
 }
